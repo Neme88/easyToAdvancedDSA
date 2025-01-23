9e22b0926870fa5cc3603f45611bb8cd31399fc7
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-23 10:33:42 +0200

    Implemented the O(1)operation code example 2

--- Big-O Notaion/O(1)CodeOperation.py
+++ Big-O Notaion/O(1)CodeOperation.py
@@ -1,4 +1,11 @@
 # implementing the first O(1) Big-O notation operation
 arr = [10, 20, 30, 40, 50]
 print(arr[2])
-# This algorithm runs in a constant time regardless the size of arr there by making it a O(1) operation.
+# Explanation: This algorithm runs in a constant time regardless the size of arr there by making it a O(1) operation.
+
+# O(1) Big-O notation operation example 2 code implementation.
+x = 45
+print(x)
+# Explanation: This code runs constant time as while checking the value of x making it an O(1) operation
+
+
