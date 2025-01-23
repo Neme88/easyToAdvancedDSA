498a3e7ef8346c10351b9e2c58024c257cc2e93d
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-23 10:46:37 +0200

    Implemented the O(1)operation code example 3

--- Big-O Notaion/O(1)CodeOperation.py
+++ Big-O Notaion/O(1)CodeOperation.py
@@ -8,4 +8,15 @@ x = 45
 print(x)
 # Explanation: This code runs constant time as while checking the value of x making it an O(1) operation
 
+#  O(1) Big-O notation operation example 2 code implementation.
+# Hash map (dictionary in python) look up implementation.
+Student_reg_nr = {"alice":"1234", "Nora":"5678", "Chris": "1011", "Ryan":"1213"}
+print(Student_reg_nr["alice"])
+"""
+Explanation: This code will always return the reg number of alice no matter the size of the hash map
+(dictionary in python) independent of the size of the dictionary because the
+dictionary mechanism is built in such a way that items get retrieved directly using it key.
+"""
+
+
 
