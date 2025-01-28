054379ac28fb43cf885a0effca04ee2a6ec39e1b
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-28 13:56:45 +0200

    included planned optimization guide for this algorithm

--- list_avarage.py
+++ list_avarage.py
@@ -12,6 +12,12 @@ def average(table=[]):
 
 # Big-O complexity is O(2n) which ends of becoming O(n) due to the constant dropping rules.
 
+# planned optimization for this algorithm includes 
+# Preprocessing input.
+# Lazy validation.
+# Precomputing and caching sums.
+# Using a running sum and count for dynamic datasets.
+
 input_One = [2,3,4,79,34,67, 12, 100, 10, 12,]
 input_Two = ['2','4','5',6]
 input_three = 78
