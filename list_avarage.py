23a764b52c9a4342b1026f107be8edce388528bd
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-01-28 13:53:40 +0200

    The avaerage number in a list of numbers calculator algorithm

--- list_avarage.py
+++ list_avarage.py
@@ -1,14 +1,16 @@
 def average(table=[]):
 
-    if not table:
+    if not table: #O(1)
         return 0
-    if not isinstance(table, list):
+    if not isinstance(table, list): #O(1)
         return str("Input must be list")
-    if len(table) == 1:
+    if len(table) == 1: #O(1)
         return table[0]
-    if not all(isinstance( item, (int, float)) for item in table):
+    if not all(isinstance( item, (int, float)) for item in table): #O(n)
         return "table must be numbers"
-    return sum(table) / len(table)
+    return sum(table) / len(table) #O(n)
+
+# Big-O complexity is O(2n) which ends of becoming O(n) due to the constant dropping rules.
 
 input_One = [2,3,4,79,34,67, 12, 100, 10, 12,]
 input_Two = ['2','4','5',6]
