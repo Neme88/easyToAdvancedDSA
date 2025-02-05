09b7c3de66aa5200ed4cf58f92235fe941c964a7
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-05 13:55:55 +0200

    implemented the devide and conquer portion of the algorithm

--- Big-O Notaion/zero_N_logN_Operation.py
+++ Big-O Notaion/zero_N_logN_Operation.py
@@ -1,7 +1,28 @@
 # the following code implements the the merge sort algorithm as an O(n* log n) time complexity
 
-def merger_sort(arr: list) -> list:
+def merge_sort(arr: list) -> list:
     if len(arr) <= 1:
         return arr
+    
+    # Divide 
+    mid = len(arr) // 2
+    left_half = merge_sort(arr[:mid])
+    right_half = merge_sort(arr[mid:])
+    
+    # Conquer or Merge
+    return merge(left_half, right_half)
+def merge(left_half: list, right_half: list) -> list:
+    result = []
+    i = 0
+    j = 0
+    
+    # merge the two half arrays
+    while i < len(left_half) and j < len(right_half):
+        if left_half[i] < right_half[j]:
+            result.append(left_half[i])
+            i += 1
+        else:
+            result.append(right_half[j])
+            j += 1
     
     
