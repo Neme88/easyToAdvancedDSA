651eb742ee6536345d8ff6a40628214b9d1998af
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-05 13:42:09 +0200

    implemented the condtion to make sure our arr has more than one element in it otherwise return empty arr or 1 item

--- Big-O Notaion/zero_N_logN_Operation.py
+++ Big-O Notaion/zero_N_logN_Operation.py
@@ -1,2 +1,7 @@
 # the following code implements the the merge sort algorithm as an O(n* log n) time complexity
 
+def merger_sort(arr: list) -> list:
+    if len(arr) <= 1:
+        return arr
+    
+    
