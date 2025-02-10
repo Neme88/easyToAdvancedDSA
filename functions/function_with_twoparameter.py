8368afe80e833d8f7c302d37338a347687fda4a6
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-02-10 17:10:15 +0200

    added more constraint to check for negative numbers and zero

--- functions/function_with_twoparameter.py
+++ functions/function_with_twoparameter.py
@@ -1,7 +1,10 @@
 # a function with two parameters
-def function_with_two_params(param1: int, param2: int) -> str:
-    return param1 + param2
-
+def function_with_two_params(param1: int, param2: int) -> int:
+    if param1  <= 0 or param2  <= 0:
+        return ("Both parameters should be greater than zero")
+    else:
+        return param1 + param2
 # testing the function
-res = function_with_two_params(10,20)
+res = function_with_two_params(0, 4)
+
 print(res)
