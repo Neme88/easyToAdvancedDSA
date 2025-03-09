e21e54346ae73a7b893d8b5aea18af6cbe5849d9
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-03-09 21:08:38 +0200

    feat: add use cases for the unordered list search algorithm

--- lists_algorithms/search_list_algorithm.py
+++ lists_algorithms/search_list_algorithm.py
@@ -1,3 +1,10 @@
 # Searching & Counting list element
 def find_index(lst, element):
     return lst.index(element) if element in lst else -1
+
+# use case
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+element_to_find = 5
+
+index = find_index(my_list, element_to_find)
+print(f'The index of {element_to_find} in the list is: {index}')
