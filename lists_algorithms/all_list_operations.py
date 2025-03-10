0d8d54d301b3bd9432ac5afa120e7277f96d8f15
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-03-10 22:09:35 +0200

    Feat: add list reaction function

--- lists_algorithms/all_list_operations.py
+++ lists_algorithms/all_list_operations.py
@@ -1,2 +1,6 @@
 from collections import deque
 from functools import reduce
+
+# addCreating and Initializing a List
+def create_list(elements=None):
+    return elements if elements is not None else []
