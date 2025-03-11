383f1d21fd3621bb1bc8511eb826994b832a4be9
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-03-11 20:42:45 +0200

    removed individual test cases

--- lists_algorithms/all_list_operations.py
+++ lists_algorithms/all_list_operations.py
@@ -5,19 +5,11 @@ from functools import reduce
 def create_list(elements=None):
     return elements if elements is not None else []
 
-# use case
-my_list = create_list([1, 2, 3, 4, 5])
-print(f'Original List: {my_list}')
-
 # Adding Elements
 def append_element(lst, element):
     lst.append(element)
     return lst
 
-
-# use case
-my_list = append_element(my_list, 6)
-print(f'List after adding element: {my_list}')
 
 if __name__ == "__main__":
     lst = create_list([1, 2, 5, 6])
