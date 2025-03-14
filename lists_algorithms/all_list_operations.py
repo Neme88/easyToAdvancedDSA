1790324f1e05d94f85b8192d41d3166b66455aac
Chinemerem Nwaka <ccnwaka1988@gmail.com>
2025-03-14 22:57:57 +0200

    add comment for readability and clarity

--- lists_algorithms/all_list_operations.py
+++ lists_algorithms/all_list_operations.py
@@ -5,20 +5,24 @@ from functools import reduce
 def create_list(elements=None):
     return elements if elements is not None else []
 
-# Adding Elements
+# 3 methods of Adding Elements to a list  
+# 1 append to the end of the list
 def append_element(lst, element):
     lst.append(element)
     return lst
-# insert elements to a list
+# insert elements to any given index of a list
 def insert_element(lst, index, element):
     lst.insert(index, element)
     return lst
+
+# 
 
 if __name__ == "__main__":
     
     lst = create_list([1, 2, 5, 6])
     append_element(lst, 3)
-    insert_element(lst, 1, 99)
+    res = insert_element(lst, 1, 99)
+    print(res)
     
     
     
